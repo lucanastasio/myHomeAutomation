db3fc979fc05e5c42218186489606d6b3b2acebf
Luca Anastasio <anastasio.lu@gmail.com>
2024-12-08 11:25:17 +0100

    working

---
+++ firmware/nightLight/lib/menu.h
@@ -0,0 +1,14 @@
+/* Copyright (c) 2024 Luca Anastasio
+ * 
+ * Single button, single LED menu
+ * 
+*/
+
+#include <Arduino.h>
+
+#ifndef MENU_H
+#define MENU_H
+
+
+
+#endif // MENU_H
---
+++ firmware/nightLight/src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+
+/* Pins
+ * IO	Peripheral	Usage
+ * PB0	OC0A		LED
+ * PB1	IN			PIR
+ * PB2	ADC1?		Vsense
+ * PB3	ADC3		Ambient light
+ * PB4	IN			User button
+*/
+
+void setup() {
+
+}
+
+void loop() {
+
+}
